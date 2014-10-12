4fc1e2d96f6faacccc517d231ecb58747ad9bf7d
Marco Giancarli <m.a.giancarli@gmail.com>
2014-10-12 18:37:28 -0400

    updated for spring 2015 semester

--- findcourses.py
+++ findcourses.py
@@ -6,7 +6,7 @@ connection.drop_database('rutgers_hidden_courses')
 database = connection['rutgers_hidden_courses']
 course_list = database['course_list']
 
-SUBJECTS_URL = 'http://sis.rutgers.edu/soc/subjects.json?semester=92014&campus=NB&level=U'
+SUBJECTS_URL = 'http://sis.rutgers.edu/soc/subjects.json?semester=12015&campus=NB&level=U'
 
 subjects_list = requests.get(SUBJECTS_URL).json()
 subject_codes = []
@@ -14,7 +14,7 @@ subject_codes = []
 for subject in subjects_list:
 	subject_codes.append(int(subject['code']))
 
-BASE_URL = 'http://sis.rutgers.edu/soc/courses.json?semester=92014&campus=NB&level=U&subject='
+BASE_URL = 'http://sis.rutgers.edu/soc/courses.json?semester=12015&campus=NB&level=U&subject='
 
 for subject in subject_codes:
     subject_courses = requests.get(BASE_URL + str(subject)).json()
